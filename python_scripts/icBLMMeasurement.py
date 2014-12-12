61b6059cdd9abd9478dcc6ad383ec2865a27357c
Christian Buhl <c.buhl@cern.ch>
2014-12-12 13:57:56 +0100

    Added comments in the icBLM script, and added notebook, to show the characteristic waveforms of a datarun

--- python_scripts/icBLMMeasurement.py
+++ python_scripts/icBLMMeasurement.py
@@ -23,85 +23,109 @@ class icBLM:
                                  't':[]},#tm.time()]},
                                  columns = ['Q', 't'])
         
+        #This number describes the amounts of triggers, between each save of the datafile.
         self.TriggersPerMeasurement =20
+        #This number describes the number of samples taken, per trigger.
         self.SamplesPerTrigger =50 #Shouldn't be changed.
+        
+        #The product of the two cannot be more than 15000. (The total memorydepth of the keithley,
+        #    recording nothing more than the time and charge.)
         self.SamplesPerMeasurement = self.TriggersPerMeasurement*self.SamplesPerTrigger
-        #The product of the two cannot be more than 15000. (The memorydepth of the keithley)
         
         
-        #Setup Keithley for measurement.
+        #Setup Keithley for measurement, and check connection.
         rm = pv.ResourceManager()
         self.kt = rm.get_instrument('GPIB::24')
         self.kt.write('*IDN?')
         print '\n\n\n', self.kt.read(), 'is initiated - Prepare for vogon poetry!\n\n\n'
         self.kt.write('*rst')
         self.kt.write('*cls')
-        #Setup system stuff
+        
+        #Setup system state
         self.kt.write('system:zcheck 0')
         self.kt.write('system:zcheck 1')
-        self.kt.write('system:zcheck 0')
-        self.kt.write('system:lsync:state 0')
+        self.kt.write('system:zcheck 0') #It's most likely unnecessary to do more than
+        # turning the zero-check off, but I quite like the clickety-sound it makes.
+        self.kt.write('system:lsync:state 0') #Turns off synchronisation with the line voltage.
+        #Important, if a high samplerate is desired.
+        
         self.kt.write('sense:function \'charge\'')
         self.kt.write('sense:charge:range 2e-6')
-        self.kt.write('sense:charge:nplc 0.01')
-        self.kt.write('sense:charge:digits 7')
-        self.kt.write('calculate:state 0')
-        self.kt.write(':display:enable 0')
-        self.kt.write(':charge:adischarge:state 1')
-        self.kt.write(':charge:adischarge:level 1.0e-6')
+        self.kt.write('sense:charge:nplc 0.01') #Sample-time; 0.01 power-line-cycles.
+        self.kt.write('sense:charge:digits 7') #Resolution set to maximum
+        self.kt.write('calculate:state 0') #Turn off all math, for speed
+        self.kt.write(':display:enable 0') #Squeeze that last bit of sample-rate out, 
+        #  by disabling the display. It leaves a nice star on the display, though.. 
         
-        #Trigger setup:
+        self.kt.write(':charge:adischarge:state 1') #Activate the auto-discharge..
+        self.kt.write(':charge:adischarge:level 1.0e-6') # .. and set the threshold.
+        
+        ###Trigger setup:
+        '''Spicy stuff, that is. The principle is as follows:
+        1) Skip the first of three layers of control; layer1. We don't need that anyway.
+        
+        2) Wait for trigger, from the external trigger input (the triglink). When that
+        fires, all hell breaks loose. Or at least, it goes on, and takes 
+        n = 'SamplesPerMeasurement' points.
+        
+        It is furthermore limited to activate the trigger more than 
+        n = 'TriggersPerMeasurement' times, to keep that thang under control!
+        Kidding aside, it's to end it in a reasonable time.
+        
+        
+        '''
         self.kt.write(':arm:layer1:source immediate')
         
-        '''Sets the number of triggers per Measurement (nMeasurement)'''
+        #Sets the number of triggers per Measurement (nMeasurement)
         self.kt.write(':arm:layer2:count ' + str(self.TriggersPerMeasurement)) 
         
         self.kt.write(':arm:layer2:source tlink')
         self.kt.write(':arm:layer2:tconfigure:asynchronous:iline 5') #Set the input
                                                             # line for the triglink
         
-        '''Sets the number of samples per trigger'''
+        #Sets the number of samples per trigger
         self.kt.write(':trigger:count ' + str(self.SamplesPerTrigger))
         
         self.kt.write('trace:points ' + str(self.SamplesPerMeasurement))
+        
+        #Sets up the timestamp. It's an absolute format (a lot of digits), and relative to the start.
         self.kt.write('trace:tstamp:format absolute')
         self.kt.write('system:tstamp:type relative')
         self.kt.write('trace:elements tstamp')
-        self.kt.write('trace:feed:control next')
+        
+        self.kt.write('trace:feed:control next') #Control, for taking succesive samples, and saving, upon trigger.
         tm.sleep(3) #COMPUTER DO SOMETHING!
     
-    #Setup plot for showing. It's still undecided what it should show.
-    #   I'm thinking two plots on top of each other, one with the primary value,
-    #   and one with the background offset signal (The latter three)
+
 filename = 'D:\\Frascati\\data\\electrometer\\Measurement_icBLM'+str(int(tm.time()))
 ic = icBLM()
 nMeasurement = 0
-while nMeasurement < 121:# 1800/ic.TriggersPerMeasurement: #Important setting - the maximum number of triggers per run
+while nMeasurement < 121:  #Important setting - the maximum number of triggers per run
+    
+    #Start the aquisition
     ic.kt.write('trace:feed:control next')
     ic.kt.write('init')
+    
     isMeasuring = True
     t0 = tm.time()
     while isMeasuring == True:
+        #The beauty. Monitors the status registers, and waits for a 'full memory'-flag.
         n = bin(int(str(ic.kt.ask(':status:measurement:event?'))[:-1]))[2:]
         try:
             if n[-10] == '1':
+                #This loop waits for the measurement to be indicated finished. When that
+                #happens, a flag is set, and the loop is finished.
                 print 'Measurement-step finished - saving in array as ' + filename
-                #Functionality for saving the recently measured array in the large array.
-                #
-                #
-                # ##################
-                #
-                #
                 q = kf.list2pandasCharge(ic.kt.ask_for_values('trace:data?'), t0)
                 ic.outArray = ic.outArray.append(q, ignore_index = True)
                 isMeasuring = False #Ends the loop.
                 
-        except IndexError:
+        except IndexError: #It'd crash a lot, if this wasn't there. As in A LOT.
             pass
     nMeasurement = nMeasurement + 1
-    ic.outArray.to_pickle(filename)
+    ic.outArray.to_pickle(filename) #Save the data. Note that this happens fairly often. Clever feature.
 
-
+#For the sake of curiosity, this enables you to see what you're actually measuring. Nifty. 
 plt.plot(ic.outArray['t'], ic.outArray['Q'], 'x')
 plt.show()
     
